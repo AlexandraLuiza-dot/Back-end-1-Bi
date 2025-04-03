1880969f541875808bde72783d9f20d1e0471aca
AlexandraLuiza-dot <alexandraluizamg@gmail.com>
2025-04-03 11:26:30 -0400

    atividades

--- HelloWorld.js
+++ HelloWorld.js
@@ -1,2 +1,18 @@
-//Meu primeiro bla bla bla
+//Meu primeiro Olá Mundo
 console.log('Hello, Word')
+
+var nome = 'cccccc'
+let nm = 'aaaaaa'
+const name = 'bbbbbbb'
+
+var valor = 20
+valor.toFixed(20);
+
+var ativo = true
+ativo.tostring();
+
+var nome = 'Hello World'
+nome.toLowerCase();
+
+
+let estados = ['RJ','SP','BH']
---
+++ atividade01.js
@@ -0,0 +1,10 @@
+//let nome = 'Alexandra Luiza';
+//let idade = '16';
+
+//console.log(`Meu nome é ${nome} e eu tenho ${idade}`)
+
+let coisa = 'Alouuuuu'
+coisa.toLowerCase();
+console.log(coisa);
+
+
